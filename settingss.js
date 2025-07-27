9d9f26e00f3b363a752da607175dae6aa54e9064
thesuryah46 <anjanasurya910@gmail.com>
2025-07-27 11:14:05 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: " malvin~d7MDIYAY#bYOzaSkpqUdAK1zRUSC X59YATgwQA-KAGTFX2A-7CHs,  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "7559941711", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
